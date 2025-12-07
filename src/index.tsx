7bc354bff30c696ec4d9197eaba5e12a765413a8
Rwiron <ruzindanawiron@gmail.com>
2025-12-07 12:20:26 +0200

    Fina

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './router'
+import './styles/index.css'
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
